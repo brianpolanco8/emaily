8f61deee9fb58befe504f35536f3b6e4d42fc64f
brianbaez <brianf.baez8@gmail.com>
2019-03-29 23:04:09 -0400

    create user model

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport')
+
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ "bye": "buddy" })
+})
+
+require('./routes/authRoutes')(app);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('server is running')
+})
+
