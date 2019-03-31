7dd1694371b82c83706b9346a412634f9f408154
brianbaez <brianf.baez8@gmail.com>
2019-03-31 19:44:33 -0400

    finish google authentication

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys')
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+})
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
@@ -18,11 +29,7 @@ passport.use(new GoogleStrategy({
                 //IF IT NOT EXIST
                 new User({ googleId: profile.id })
                     .save()
-                    .then(user => done(null, user))
+                    .then((user) => done(null, user))
         })
-
-
-
-
 }));
 
