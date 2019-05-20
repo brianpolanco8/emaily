506bae33a7c8efc7071b5f6ee429e561e5ad66ca
brianbaez <brianf.baez8@gmail.com>
2019-05-20 12:19:35 -0400

    changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+const Header = () => <h2>Header</h2>;
+const Dashboard= () => <h2>Dashboard</h2>;
+const SurveryNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 export default class App extends Component {
 
   render() {
+
     return (
       <div>
-        <h1>Hi Brian</h1>
+        <BrowserRouter>
+          <div>
+            {console.log(this.props)}
+            <Route path={'/'} exact component={Landing}/>
+            <Route path={'/surveys'} component={Dashboard}/>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
