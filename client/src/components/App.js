15e55a9d99664e2813474970972158e6f7411393
brianbaez <brianf.baez8@gmail.com>
2019-05-21 20:31:54 -0400

    changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Route} from 'react-router-dom'
 const Header = () => <h2>Header</h2>;
 const Dashboard= () => <h2>Dashboard</h2>;
 const SurveryNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2><a href='auth/google'>Login</a>Landing</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 export default class App extends Component {
 
