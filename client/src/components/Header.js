b66f5600dbd7b78fdbb3250c07764e3ba0be9c9b
brianbaez <brianf.baez8@gmail.com>
2019-06-04 14:00:43 -0400

    Chapter 99

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
+import Payments from './Payments'
 
 class Header extends Component {
     renderContent() {
@@ -9,20 +10,14 @@ class Header extends Component {
                 return;
 
             case false:
-                return (
-                  <li>
-                     <a href={`auth/google`} >Login with google</a>
-                  </li>
-                );
+                return    <li> <a href={`auth/google`} >Login with google</a> </li>
 
             default:
-                return (
-                    <li>
-                        <a href={`api/logout`}>
-                            Logout
-                        </a>
-                    </li>
-                )
+                return [
+                    <li><Payments /></li>,
+                    <li><a href={`api/logout`}>Logout</a></li>
+                ]
+
         }
     }
 
