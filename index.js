09c864033002fd5cc50b7b58c40a320db1c821f3
brianbaez <brianf.baez8@gmail.com>
2019-06-23 00:37:29 -0400

    had buid instead of build in index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production'){
 
   const path = require('path');
   app.get('*', ( req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'buid', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 
 }
