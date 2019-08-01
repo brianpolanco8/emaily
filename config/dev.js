75a40b18d04d764cd3aa7b1a5c5965c719af7636
brianbaez <brianf.baez8@gmail.com>
2019-08-01 13:49:38 -0400

    Chapter 150

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: '485633166503-qsinpqrbqk233jbc9pllkalpnq8guhf8.apps.googleusercontent.com',
+    googleClientSecret: 'yXkkAYx-JBFOSrTygjA8nZQP',
+    mongoURI: 'mongodb+srv://root:root@emaily-5nf18.mongodb.net/emaily?retryWrites=true',
+    cookieKey: 'asdasdasdasdasdasdsasdsaqwe',
+    stripePublishableKey: 'pk_test_0vyWgnq0wgZhW0ZUblal1FNv00ueMC2kW8',
+    stripeSecretKey: 'sk_test_1l6Le5a170fPfrYkiQ9XKRIB00Bbnb47ZR',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'
+};
