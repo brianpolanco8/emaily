f6b5ff4b81a27aecd440013b027924ac410768a2
brianbaez <brianf.baez8@gmail.com>
2019-08-06 21:52:32 -0400

    Chapter 158

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,25 @@
+// import _ from 'lodash'
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form'
 import SurveyField from './SurveyField'
+import { Link } from 'react-router-dom'
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'recipient' }
+]
+
 
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field
-                    name="surveyTitle"
-                    type="text"
-                    component={SurveyField}
-                />
-            </div>
-        )
+        return (FIELDS.map((field, i) => {
+            return (
+                <Field component={SurveyField} type="text" label={field.label} name={field.name} key={i} />
+            )
+        }))
+
     }
 
     render() {
@@ -20,7 +27,10 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <button type="submit" className={'btn right'}>Submit<i className="material-icons right">done</i></ button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
                 </form>
             </div>
         )
