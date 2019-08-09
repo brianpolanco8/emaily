4ccacd31f47f9fe0346a4b6267fc0d0a4ceadaee
brianbaez <brianf.baez8@gmail.com>
2019-08-08 23:19:30 -0400

    Chapter 163

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form'
 import SurveyField from './SurveyField'
 import { Link } from 'react-router-dom'
+import validateEmails from '../../utils/validateEmails'
 
 const FIELDS = [
     { label: 'Survey Title', name: 'title' },
     { label: 'Subject Line', name: 'subject' },
     { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'recipient' }
+    { label: 'Recipient List', name: 'emails' }
 ]
 
 
@@ -39,11 +40,13 @@ class SurveyForm extends Component {
 
 const validate = values => {
     const errors = {};
+    errors.emails = validateEmails(values.emails || '')
     FIELDS.forEach(({ name }) => {
         if (!values[name]) {
             errors[name] = 'Required'
         };
     });
+
     return errors;
 }
 
