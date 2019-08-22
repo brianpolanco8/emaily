606486c2c6743659bde458d58b667fde812acb34
brianbaez <brianf.baez8@gmail.com>
2019-08-22 19:17:32 -0400

    Chapter 199

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-const bodyParser = require('body-parser')
-require('./models')
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+require("./models");
+require("./services/passport");
 const app = express();
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
+mongoose.connect("mongodb://localhost:27017/emaily", { useNewUrlParser: true });
+// mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
-
-// mongoose.connect("mongodb://localhost:27017/emaily", { useNewUrlParser: true });
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-    res.send({ "bye": "buddy" })
+app.get("/", (req, res) => {
+  res.send({ bye: "buddy" });
 });
 
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-require('./routes/surveyRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('server is running')
+  console.log("server is running");
 });
